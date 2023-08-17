9566ff3638c8af7723c971d62d39692ab74e10d0
Shun Sato <shun-sato@yoriso.com>
2023-08-17 21:32:57 +0900

    Ui/Rating Propsを追加

--- components/Ui/Rating.vue
+++ components/Ui/Rating.vue
@@ -1,19 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Props {
+  size: 's' | 'm';
+  rating: number;
+}
+
+const { size, rating } = defineProps<Props>();
+</script>
 
 <template>
   <div class="d-flex align-center">
-    <v-rating :model-value="5" half-increments readonly>
+    <v-rating :model-value="rating" half-increments readonly>
       <template v-slot:item="props">
         <v-icon
           color="#F26A0C"
-          size="16"
+          :size="size === 'm' ? 16 : 12"
           :class="props.index !== 9 ? `mr-1` : ''"
         >
           {{ props.isFilled ? 'fa fa-star' : 'fa-regular fa-star' }}
         </v-icon>
       </template>
     </v-rating>
-    <span class="ml-2 font-weight-bold text-subtitle-1"> 5.0 </span>
+    <span class="ml-2 font-weight-bold text-subtitle-1">
+      {{ Number(rating).toFixed(1) }}
+    </span>
   </div>
 </template>
 
