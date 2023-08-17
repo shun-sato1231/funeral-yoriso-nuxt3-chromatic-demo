58029bdbe6a7206fcfc89e28faa673e863921cd5
Shun Sato <shun-sato@yoriso.com>
2023-08-17 21:12:13 +0900

    Ui/Rating コンポーネント表示

--- components/Ui/Rating.vue
+++ components/Ui/Rating.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts"></script>
 
 <template>
-  <span>UI Rating実装中</span>
+  <div class="d-flex align-center">
+    <v-rating :model-value="5" half-increments readonly>
+      <template v-slot:item="props">
+        <v-icon
+          color="#F26A0C"
+          size="16"
+          :class="props.index !== 9 ? `mr-1` : ''"
+        >
+          {{ props.isFilled ? 'fa fa-star' : 'fa-regular fa-star' }}
+        </v-icon>
+      </template>
+    </v-rating>
+    <span class="ml-2 font-weight-bold text-subtitle-1"> 5.0 </span>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.v-rating::v-deep .v-rating__item label {
+  display: flex;
+}
+</style>
